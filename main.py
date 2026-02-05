c25a2d97d897216eddcecff4e31ad6b91662d374
Vertig0-rgb <cagli.eran17@gmail.com>
2026-02-05 16:37:02 +0100

    ajout des 2 tableaux "game" et "mid_game"

---
+++ main.py
@@ -0,0 +1,23 @@
+#Eran cagli
+#05.02.2026
+# projet 2048 MA-20
+
+import tkinter as tk
+from tkinter import mainloop
+
+game = [[0,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,0]]
+
+
+mid_game = [[0,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,0]]
+
+window = tk.Tk()
+window.geometry("600x600")
+window.title("MA20_2048_Cagli Eran")
+
+mainloop()
